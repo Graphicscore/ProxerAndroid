612c94da0e354973bb4ea07eeefeb2fd3e1883db
Asteria Louven <asteria@louven.me>
2026-07-12 08:57:40 +0200

    style(media): fix import order in MediaScreen.kt
    Category import broke alphabetical grouping introduced in the
    previous commit; detekt flagged it as new noise.

--- src/main/kotlin/me/proxer/app/media/MediaScreen.kt
+++ src/main/kotlin/me/proxer/app/media/MediaScreen.kt
@@ -27,7 +27,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import kotlinx.coroutines.launch
 import me.proxer.app.R
-import me.proxer.library.enums.Category
 import me.proxer.app.media.comments.CommentsScreen
 import me.proxer.app.media.discussion.DiscussionScreen
 import me.proxer.app.media.episode.EpisodeScreen
@@ -35,6 +34,7 @@ import me.proxer.app.media.info.MediaInfoScreen
 import me.proxer.app.media.recommendation.RecommendationScreen
 import me.proxer.app.media.relation.RelationScreen
 import me.proxer.app.ui.compose.ProxerTheme
+import me.proxer.library.enums.Category
 import org.koin.androidx.compose.koinViewModel
 import org.koin.core.parameter.parametersOf
 
