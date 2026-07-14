39d318cc845b867fe8d7d33320f0495f2fe5af12
Asteria Louven <asteria@louven.me>
2026-07-14 15:52:17 +0200

    chore(test) fix kotlin test

--- src/androidTest/kotlin/me/proxer/app/tv/TvErrorViewTest.kt
+++ src/androidTest/kotlin/me/proxer/app/tv/TvErrorViewTest.kt
@@ -1,7 +1,7 @@
 package me.proxer.app.tv
 
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.v2.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
